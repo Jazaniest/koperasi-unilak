1d1730791827acfbc1150fb6c788dfc789d84f97
Jazaniest <Jazaniezt07@gmail.com>
2026-06-13 08:58:10 +0700

    menambahkan component untuk admin

---
+++ src/components/admin/AdminNavbar.jsx
@@ -0,0 +1,6 @@
+import { IconHome, IconUsers } from '../ui/Icons'
+
+export const AdminNavbar = [
+    { to: '/admin', label: 'Dashboard', icon: IconHome, end: true },
+    { to: '/admin/anggota', label: 'Anggota', icon: IconUsers },
+]
