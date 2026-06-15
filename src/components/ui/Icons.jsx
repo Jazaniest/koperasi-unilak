42b9e1fc0b982e563171037733e8c0d77a753c6f
Jazaniest <Jazaniezt07@gmail.com>
2026-06-15 18:10:13 +0700

    menambahkan iconrefresh, iconhistory, iconnews

--- src/components/ui/Icons.jsx
+++ src/components/ui/Icons.jsx
@@ -130,3 +130,28 @@ export function IconUserEdit({ className = 'w-4 h-4' }) {
     </svg>
   )
 }
+
+export function IconRefresh({ className = 'w-5 h-5' }) {
+  return (
+    <svg className={className} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" xmlns="http://www.w3.org/2000/svg">
+      <path d="M12 2 A10 10 0 1 1 5.05 18.07" />
+      <polyline points="1.5,13 5.05,18.07 10.5,15.2" />
+    </svg>
+  )
+}
+
+export function IconHistory({ className = 'w-5 h-5' }) {
+    return (
+        <svg className={className} fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+        </svg>
+    )
+}
+
+export function IconNews({ className = 'h-5 w-5' }) {
+  return (
+    <svg className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
+      <path strokeLinecap="round" strokeLinejoin="round" d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 01-2.25 2.25M16.5 7.5V18a2.25 2.25 0 002.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 002.25 2.25h13.5M6 7.5h3v3H6v-3z" />
+    </svg>
+  )
+}
