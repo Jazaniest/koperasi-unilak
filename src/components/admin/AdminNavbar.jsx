5d1e0b901f988110254edf256bae46ff8c8bbc6e
Jazaniest <Jazaniezt07@gmail.com>
2026-06-15 18:07:55 +0700

    menambahkan untuk berita

--- src/components/admin/AdminNavbar.jsx
+++ src/components/admin/AdminNavbar.jsx
@@ -1,7 +1,8 @@
-import { IconHome, IconUsers } from '../ui/Icons'
+import { IconHome, IconUsers, IconNews } from '../ui/Icons'
 
 export const AdminNavbar = [
     { to: '/admin', label: 'Dashboard', icon: IconHome, end: true },
     { to: '/admin/anggota', label: 'Anggota', icon: IconUsers },
     { to: '/admin/pendaftaran', label: 'Pendaftaran', icon: IconUsers },
+    { to: '/admin/berita', label: 'Berita', icon: IconNews },
 ]
